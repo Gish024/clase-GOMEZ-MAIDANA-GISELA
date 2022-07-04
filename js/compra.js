67b43c9612b09c57dccf9183ece74d643d60bc69
Gisela Gomez <gisela865504@gmail.com>
2022-07-04 16:03:59 -0300

    modificando-carrito

--- js/compra.js
+++ js/compra.js
@@ -9,326 +9,257 @@ saludo.innerText = '¡Hola ' +saludoCliente+ ' selecciona los productos que quie
 
 console.log( saludo.innerText ) 
 
+// OBJETOS y ARRAYS
+
+const array = [
+  {
+    id: 1,
+    name: "Medallones de Garbanzos",
+    img: "../resources/medallongarbanzo.jpg",
+    precio: 600,
+  },
+  {
+    id: 2,
+    name: "Medallones de Arroz Yamaní y espinaca",
+    img: "../resources/medallonarroz.jpg",
+    precio: 600,
+  },
+  {
+    id: 3,
+    name: "Medallones de Lenteja",
+    img: "../resources/medallon.jpg",
+    precio: 600,
+  },
+  {
+    id: 4,
+    name: "Tarta de choclo",
+    img: "../resources/tartaschoclo.jpg",
+    precio: 800,
+  },
+  {
+    id: 5,
+    name: "Tarta de espinaca",
+    img: "../resources/tartaespinaca.jpg",
+    precio: 800,
+  },
+  {
+    id: 6,
+    name: "Albondigas de Lentejas",
+    img: "../resources/albondigas.jpg",
+    precio: 500,
+  },
+  {
+    id: 7,
+    name: "Prepizza de Zanahoria",
+    img: "../resources/pizzacaserazanahoria.jpg",
+    precio: 400,
+  },
+  {
+    id: 8,
+    name: "Panqueques de espinaca",
+    img: "../resources/panquequesespinaca.jpg",
+    precio: 300,
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+primerCompraGet();
+segundaCompraGet() 
+});
 
 
+// -- localStorage -- 
 
+const primerCompraGet = () => {
+if (localStorage.getItem("compra")) {
+  compra = JSON.parse(localStorage.getItem("compra"));
+  pintarCarrito();
+}
+};
 
-import { actualizarCarrito } from "./carritoActualizado.js";
-import { productos } from "./stock.js";
-  
+// --- PINTAR CARRITO ---
 
+const templateCarrito = document.querySelector("#template-carrito").content;
+const fragment = document.createDocumentFragment();
 
+const listaProductos = document.querySelector(".list-products"); 
 
+// - Donde se pintaran nuestros costos
 
-export const carritoIndex = (productoId) => {
-  let carritoDeCompras = [];
-    if (localStorage.getItem("carrito")) {
-      carritoDeCompras = obtenerCarritoStorage();
-    }
-    let productoRepetido = carritoDeCompras.find(producto => producto.id == productoId);
-    contarProductosRepetidos(productoRepetido, productoId, carritoDeCompras);
+const carritoSubTotal = document.querySelector(".carrito-subTotal");
+const carritoEnvio = document.querySelector(".carrito-envio");
+const carritoIva = document.querySelector(".carrito-iva");
+const carritoTotal = document.querySelector(".carrito-total");
+
+const pintarCarrito = () => {
+
+listaProductos.innerHTML = ""; 
+Object.values(compra).forEach((producto) => {
+  if (producto.price > 0) {
+    
+    templateCarrito.querySelectorAll("td")[0].textContent = producto.name;
+    templateCarrito.querySelectorAll("td")[1].textContent = producto.amount;
+    templateCarrito.querySelectorAll("td")[2].textContent = producto.amount * producto.price;
+
+    const clone = templateCarrito.cloneNode(true);
+    fragment.appendChild(clone);
   }
-  
-  const contarProductosRepetidos = (prodRepetido, productoId, carritoDeCompras) => {
-    if (prodRepetido) {
-      prodRepetido.cantidad++
-      document.getElementById(`cantidad${prodRepetido.id}`).innerHTML = `<p id=cantidad${prodRepetido.id}>Cantidad:${prodRepetido.cantidad}</p>`;
-      actualizarCarrito(carritoDeCompras);
-    } else {
-      agregarProductoAlCarrito(productoId, carritoDeCompras);
-    }
+});
+
+listaProductos.appendChild(fragment);
+
+pintarCostos();
+};
+
+// --- Costos ---
+
+const pintarCostos = () => {
+const subTotal = Object.values(compra).reduce(
+  (acc, { amount, price }) => acc + price * amount,
+  0
+);
+
+carritoSubTotal.textContent = "$ " + subTotal;
+
+const iva = subTotal * 0.21;
+carritoIva.textContent = "$ " + Math.trunc(iva);
+
+let envio = 0;
+
+const mostrarEnvio = () => {
+  if (total > 3000) {
+    envio = 0;
+  } else {
+    envio = 200;
   }
-  
-  const agregarProductoAlCarrito = (productoId, carritoDeCompras) => {
-    const contenedor = document.getElementById('carrito-contenedor');
-    const producto = productos.find(producto => producto.id == productoId);
-    carritoDeCompras.push(producto);
-  
-    producto.cantidad = 1;
-  
-    const div = document.createElement('div');
-    div.classList.add('productoEnCarrito');
-    div.innerHTML = ` <p>${producto.nombre}</p>
-                      <p>Precio:${producto.precio}</p>
-                      <p id=cantidad${producto.id}>Cantidad:${producto.cantidad}</p>
-                      <button id=eliminar${producto.id} class="btn waves-effect waves-light boton-eliminar" value="${producto.id}">X</i></button>
-                    `
-    contenedor.appendChild(div);
-    actualizarCarrito(carritoDeCompras);
-  };
-  
-  export const eliminarProductoCarrito = (productoId) => {
-    const carritoStorage = obtenerCarritoStorage();
-    const carritoActualizado = carritoStorage.filter(el => el.id != productoId);
-  
-    actualizarCarrito(carritoActualizado);
-    renderProductosCarrito(carritoActualizado);
-  };
-  
-  export const renderProductosCarrito = (carritoDeCompras) => {
-    const contenedor = document.getElementById('carrito-contenedor');
-  
-    contenedor.innerHTML = "";
-  
-    carritoDeCompras.forEach(producto => {
-      const div = document.createElement('div');
-      div.classList.add('productoEnCarrito');
-      div.innerHTML = ` <p>${producto.nombre}</p>
-                        <p>Precio:${producto.precio}</p>
-                        <p id=cantidad${producto.id}>Cantidad:${producto.cantidad}</p>
-                        <button id=eliminar${producto.id} class="btn waves-effect waves-light boton-eliminar" value="${producto.id}">X</button>
-                      `
-      contenedor.appendChild(div);
-    });
-  };
-  
-  export const obtenerCarritoStorage = () => {
-    const carritoStorage = JSON.parse(localStorage.getItem("carrito"))
-    return carritoStorage;
-  }
-  
+};
 
+const total = subTotal + iva + envio;
+carritoTotal.textContent = "$ " + total;
 
+mostrarEnvio();
+carritoEnvio.textContent = "$ " + envio;
+};
 
+const template = document.querySelector("#template").content;
+const cards = document.querySelector("#items");
 
+// PINTAR CARDS 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
- /* document.addEventListener("DOMContentLoaded", () => {
-    primerCompraGet();
-    segundaCompraGet() 
-  });
-
-
-  // -- localStorage -- 
-
-  const primerCompraGet = () => {
-    if (localStorage.getItem("compra")) {
-      compra = JSON.parse(localStorage.getItem("compra"));
-      pinCarr();
-    }
-  };
-
-   // --- PINTAR CARRITO ---
-  
-  const carrito = document.querySelector("#carrito").content;
-  const fragmento = document.createDocumentFragment();
-  
-  const listaProductos = document.querySelector(".list-products"); 
-  
-  // - Donde se pintaran nuestros costos
-
-  const carrSubTotal = document.querySelector(".carrito-subTotal");
-  const carrEnvio = document.querySelector(".carrito-envio");
-  const carrIva = document.querySelector(".carrito-iva");
-  const carrTotal = document.querySelector(".carrito-total");
-  
-  const pinCarr = () => {
-
-    listaProductos.innerHTML = ""; 
-    Object.values(compra).forEach((producto) => {
-      if (producto.price > 0) {
-        
-        carrito.querySelectorAll("h5")[0].textContent = producto.name;
-        carrito.querySelectorAll("p")[1].textContent = producto.amount;
-        carrito.querySelectorAll("a")[2].textContent = producto.amount * producto.price;
-  
-        const clone = carrito.cloneNode(true);
-        fragmento.appendChild(clone);
-      }
-    });
-
-    listaProductos.appendChild(fragmento);
-  
-    pinCostos();
-  };
-  
-  // --- Costos ---
-
-  const pinCostos = () => {
-    const subTotal = Object.values(compra).reduce(
-      (acc, { amount, price }) => acc + price * amount,
-      0
-    );
-  
-    carrSubTotal.textContent = "$ " + subTotal;
-  
-    const iva = subTotal * 0.21;
-    carrIva.textContent = "$ " + Math.trunc(iva);
-  
-    let envio = 0;
-  
-    const mostrarEnvio = () => {
-      if (total > 3000) {
-        envio = 0;
-      } else {
-        envio = 200;
-      }
-    };
-  
-    const total = subTotal + iva + envio;
-    carrTotal.textContent = "$ " + total;
-  
-    mostrarEnvio();
-    carrEnvio.textContent = "$ " + envio;
-  };
-  
-   
-  const cards = document.querySelector("#carrito div");
-  
-  // PINTAR CARDS 
-  
-  function showProducts(array) {
+const showProduct = (array) => {
   array.forEach((el) => {
     if (el.precio > 0) {
-      carrito.querySelector(".card-title").textContent = el.name;
-      carrito.querySelector(".card-text").textContent = el.precio;    
-      carrito.querySelector(".btn-compra").dataset.id = el.id;
-      const clone = carrito.cloneNode(true);
-      fragmento.appendChild(clone);
+      template.querySelector("#cTitle").textContent = el.name;
+      template.querySelector("#cText").textContent = el.precio;
+      template.querySelector("#card-img").setAttribute("src", el.img);
+      template.querySelector("#btn-compra").dataset.id = el.id;
+      const clone = template.cloneNode(true);
+      fragment.appendChild(clone);
     }
   });
-  cards.appendChild(fragmento);
-}
+  cards.appendChild(fragment);
+};
+
+//--- SELECCION DE PRODUCTOS ---
   
-  //--- SELECCION DE PRODUCTOS ---
+let compraItems = {};
   
-  let compraItems = {};
-  
-  // --- AGREGAR AL CARRITO ---
-  cards.addEventListener("click", (e) => {
-    addCarrito(e);
-  });
-  
-  const addCarrito = (e) => {
-    if (e.target.classList.contains("btn-compra")) {
-      setCarrito(e.target.parentElement);
-    }
-  
-    e.stopPropagation();
-  };
-  
-  const setCarrito = (objeto) => {
-    const producto = {
-      id: objeto.querySelector(".btn-compra").dataset.id,
-      name: objeto.querySelector(".card-title").textContent,
-      price: objeto.querySelector(".card-text").textContent,
-      amount: 1,
-    };
-  
-    if (compraItems.hasOwnProperty(producto.id)) {
-      producto.amount = compraItems[producto.id].amount + 1;
-    }
-  
-    compraItems[producto.id] = { ...producto };
-    pintaCarro();
+// --- AGREGAR AL CARRITO ---
+cards.addEventListener("click", (e) => {
+  addCarrito(e);
+});
+
+const addCarrito = (e) => {
+  if (e.target.classList.contains("btn-primary")) {
+    setCarrito(e.target.parentElement);
+  }
+
+  e.stopPropagation();
+};
+
+const setCarrito = (objeto) => {
+  const producto = {
+    id: objeto.querySelector("#btn-compra").dataset.id,
+    name: objeto.querySelector("#cTitle").textContent,
+    price: objeto.querySelector("#cText").textContent,
+    amount: 1,
   };
 
-  const carritoDos = document.querySelector(".card").content;  
-  const lista = document.querySelector(".list-product");
+  if (compraItems.hasOwnProperty(producto.id)) {
+    producto.amount = compraItems[producto.id].amount + 1;
+  }
 
+  compraItems[producto.id] = { ...producto };
+  pintarSegundoCarrito();
+};
 
-  
-  function pintaCarro() {
-  lista.innerHTML = "";
+// --- PINTAR SEGUNDO CARRITO --- (de productos individuales)
+
+const templateCarritoDos = document.querySelector(
+  "#template-segundo-carrito"
+).content;
+
+const lista = document.querySelector("#list-items");
+
+const pintarSegundoCarrito = () => {
+  lista.innerHTML = ""; // evitamos que recarge nuevamente los productos
   Object.values(compraItems).forEach((producto) => {
     if (producto.price > 0) {
-      
-      carritoDos.querySelectorAll("h5")[0].textContent = producto.name;
-      carritoDos.querySelectorAll("p")[1].textContent = producto.amount;
-      carritoDos.querySelectorAll("a")[2].textContent = producto.amount * producto.price;
+      // evitamos que muestre los productos con precio > 0
+      templateCarritoDos.querySelectorAll("td")[0].textContent = producto.name;
+      templateCarritoDos.querySelectorAll("td")[1].textContent = producto.amount;
+      templateCarritoDos.querySelectorAll("td")[2].textContent = producto.amount * producto.price;
 
-      const clone = carritoDos.cloneNode(true);
-      fragmento.appendChild(clone);
+      const clone = templateCarritoDos.cloneNode(true);
+      fragment.appendChild(clone);
     }
   });
-  lista.appendChild(fragmento);
+  lista.appendChild(fragment);
 
-  pinItems();
-  segundaCompra();
-}
-  
-  // --- Pintamos los costos del segundo carrito --
-  
-  const pinItems = () => {
-    const subTotal = Object.values(compraItems).reduce(
-      (acc, { amount, price }) => acc + price * amount,
-      0
-    );
-  
-    carritoSubTotal.textContent = "$ " + subTotal;
-  
-    const iva = subTotal * 0.21;
-    carritoIva.textContent = "$ " + Math.trunc(iva);
-  
-    let envio = 0;
-  
-    const mostrarEnvio = () => {
-      if (total > 3000) {
-        envio = 0;
-      } else {
-        envio = 200;
-      }
-    };
-    const total = subTotal + iva + envio;
-    carritoTotal.textContent = "$ " + total;
-  
-    mostrarEnvio();
-    carritoEnvio.textContent = "$ " + envio;
-  };
-  
-  // LOCALSTORAGE
-  const segundaCompra = () => {
-    localStorage.setItem("productos", JSON.stringify(compraItems));
-  };
-  
-  const segundaCompraGet = () => {
-    if (localStorage.getItem("productos")) {
-      compraItems = JSON.parse(localStorage.getItem("productos"));
-      pintaCarro();
+  pintarCostosItems();
+  segundaCompraStorage();
+};
+
+// --- Pintamos los costos del segundo carrito --
+
+const pintarCostosItems = () => {
+  const subTotal = Object.values(compraItems).reduce(
+    (acc, { amount, price }) => acc + price * amount,
+    0
+  );
+
+  carritoSubTotal.textContent = "$ " + subTotal;
+
+  const iva = subTotal * 0.21;
+  carritoIva.textContent = "$ " + Math.trunc(iva);
+
+  let envio = 0;
+
+  const mostrarEnvio = () => {
+    if (total > 3000) {
+      envio = 0;
+    } else {
+      envio = 200;
     }
-  };*/
+  };
+  const total = subTotal + iva + envio;
+  carritoTotal.textContent = "$ " + total;
+
+  mostrarEnvio();
+  carritoEnvio.textContent = "$ " + envio;
+};
+
+// --- LOCALSTORAGE 2DO CARRITO ---
+const segundaCompraStorage = () => {
+  localStorage.setItem("productos", JSON.stringify(compraItems));
+};
+
+const segundaCompraGet = () => {
+  if (localStorage.getItem("productos")) {
+    compraItems = JSON.parse(localStorage.getItem("productos"));
+    pintarSegundoCarrito();
+  }
+};
 
 
   
