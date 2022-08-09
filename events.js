ea74e20de7a655abba273b4a6b11cb056f3f5423
Gisela Gomez <gisela865504@gmail.com>
2022-08-09 16:40:02 -0300

    Colores

--- events.js
+++ events.js
@@ -4,34 +4,33 @@ const form = document.querySelector("#form");
 const DateTime = luxon.DateTime;
 
 const now = DateTime.now();
-const llegada = DateTime.fromObject({ day: now.day + 10 });
 
 form.addEventListener("submit", (e) => {
   const name = document.querySelector("#validationDefault01").value;
   const lastName = document.querySelector("#validationTooltip02").value;
   e.preventDefault();
   e.stopPropagation();
-  Swal.fire({
-    title: `¡Gracias por confiar en PURO SABOR! ${name} ${lastName}`,
-    html: `<span class="sweet-fechas">Fecha actual: ${now.toLocaleString()}</span>
-          <span class="sweet-fechas pt-2">Te enviamos un correo electronico con el detalle de tu compra.</span>
-          <span class="sweet-fechas pt-3">Fecha de entrega aproximada: ${llegada.toLocaleString()}</span>`,
-    color: "#fff",
-    confirmButtonText: "Continuar",
-    confirmButtonColor: "#ffb320",
-    imageUrl: "../resources/logo.jpg",
-    imageAlt: "PURO SABOR LOGO",
-    imageWidth: 150,
-    imageHeight: 80,
-    background: "#1f2225",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      window.location.href = "../index.html";
-    }
+    Swal.fire({
+      title: `¡Gracias por confiar en PURO SABOR! ${name} ${lastName}`,
+      html: `<span class="sweet-fechas">Fecha: ${now.toLocaleString()}</span>
+             <br>
+            <span class="sweet-fechas pt-2"> A la brevedad te enviaremos un correo electrónico con todos los detalles.</span>`,             
+      color: "rgb(108, 106, 106)",
+      confirmButtonText: "Continuar",
+      confirmButtonColor: "#567bf5",
+      imageUrl: "../resources/logo.jpg",
+      imageAlt: "PURO SABOR LOGO",
+      imageWidth: 150,
+      imageHeight: 150,
+      background: "#96efa5",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "../index.html";
+      }
+    });
+    
+    setTimeout(() => (window.location.href = "../index.html"), 15000);
   });
-  setTimeout(() => (window.location.href = "../index.html"), 15000);
-  deleteStorage();
-});
 
 const forms = document.querySelectorAll('.needs-validation')
 
